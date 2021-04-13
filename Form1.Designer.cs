451b1811fcaf37b6e0b825641072393d82f5a1b6
gagicanja <anjagagic3@gmail.com>
2021-04-13 18:54:35 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Projekat
+{
+    partial class DePicto
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCrtanje = new System.Windows.Forms.Panel();
+            this.picOlovka = new System.Windows.Forms.PictureBox();
+            this.picGumica = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picOlovka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGumica)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlCrtanje
+            // 
+            this.pnlCrtanje.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlCrtanje.BackColor = System.Drawing.Color.White;
+            this.pnlCrtanje.Location = new System.Drawing.Point(0, 39);
+            this.pnlCrtanje.Name = "pnlCrtanje";
+            this.pnlCrtanje.Size = new System.Drawing.Size(800, 411);
+            this.pnlCrtanje.TabIndex = 0;
+            this.pnlCrtanje.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCrtanje_Paint);
+            this.pnlCrtanje.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlCrtanje_MouseDown);
+            this.pnlCrtanje.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlCrtanje_MouseMove);
+            this.pnlCrtanje.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlCrtanje_MouseUp);
+            // 
+            // picOlovka
+            // 
+            this.picOlovka.Image = global::Projekat.Properties.Resources.pencil;
+            this.picOlovka.Location = new System.Drawing.Point(0, 1);
+            this.picOlovka.Name = "picOlovka";
+            this.picOlovka.Size = new System.Drawing.Size(36, 36);
+            this.picOlovka.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOlovka.TabIndex = 0;
+            this.picOlovka.TabStop = false;
+            this.picOlovka.Click += new System.EventHandler(this.picOlovka_Click);
+            // 
+            // picGumica
+            // 
+            this.picGumica.Image = global::Projekat.Properties.Resources.eraser;
+            this.picGumica.Location = new System.Drawing.Point(42, 1);
+            this.picGumica.Name = "picGumica";
+            this.picGumica.Size = new System.Drawing.Size(36, 36);
+            this.picGumica.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGumica.TabIndex = 1;
+            this.picGumica.TabStop = false;
+            this.picGumica.Click += new System.EventHandler(this.picGumica_Click);
+            // 
+            // DePicto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picGumica);
+            this.Controls.Add(this.picOlovka);
+            this.Controls.Add(this.pnlCrtanje);
+            this.Name = "DePicto";
+            this.Text = "DePicto";
+            ((System.ComponentModel.ISupportInitialize)(this.picOlovka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGumica)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCrtanje;
+        private System.Windows.Forms.PictureBox picOlovka;
+        private System.Windows.Forms.PictureBox picGumica;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekat
+{
+    public partial class DePicto : Form
+    {
+        Graphics g;
+        Pen p;
+        bool pomeranje = false;
+        int x, y;
+        Color trenBoja;
+        int trenVelicina;
+        public DePicto()
+        {
+            InitializeComponent();
+            g = pnlCrtanje.CreateGraphics();
+            trenVelicina = 3;
+            p = new Pen(Color.Black, trenVelicina);
+            x = -1; y = -1;
+            trenBoja = Color.Black;
+        }
+
+        private void pnlCrtanje_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnlCrtanje_MouseDown(object sender, MouseEventArgs e)
+        {
+            pomeranje = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void pnlCrtanje_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pomeranje && x != -1 && x != -1)
+            {
+                g.DrawLine(p, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void picOlovka_Click(object sender, EventArgs e)
+        {
+            p = new Pen(trenBoja, trenVelicina);
+        }
+
+        private void picGumica_Click(object sender, EventArgs e)
+        {
+            p = new Pen(BackColor, 30);
+        }
+
+        private void pnlCrtanje_MouseUp(object sender, MouseEventArgs e)
+        {
+            pomeranje = false;
+            x = -1;
+            y = -1;
+        }
+    }
+}
+
+
+
